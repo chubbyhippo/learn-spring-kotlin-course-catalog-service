fbd0e0cc26ce582e8bde3cf9abe71e31837662a7
ch <mkmsacc@outlook.com>
2022-03-20 15:50:49 +0700

    Verify function called

--- src/test/kotlin/com/example/coursecatalogservice/controller/GreetingsControllerTest.kt
+++ src/test/kotlin/com/example/coursecatalogservice/controller/GreetingsControllerTest.kt
@@ -1,8 +1,12 @@
 package com.example.coursecatalogservice.controller
 
+import com.example.coursecatalogservice.service.GreetingsService
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @WebMvcTest
@@ -10,6 +14,16 @@ internal class GreetingsControllerTest {
 
     @Autowired
     private lateinit var webTestClient: WebTestClient
+
+    @MockBean
+    private lateinit var greetingsService: GreetingsService
+
+    @BeforeEach
+    fun setup() {
+        val name = "Hippo"
+        `when`(greetingsService.greet(name))
+            .thenReturn("Hello Hippo")
+    }
 
     @Test
     fun shouldReturnHello() {
@@ -21,6 +35,8 @@ internal class GreetingsControllerTest {
             .expectBody(String::class.java)
             .isEqualTo("Hello Hippo")
 
+        verify(greetingsService, times(1))
+            .greet("Hippo")
     }
 
 }
