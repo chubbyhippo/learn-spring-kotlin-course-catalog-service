044de356adb5aca3d1c08c8efabebaa429efaeeb
ch <mkmsacc@outlook.com>
2022-03-21 14:00:16 +0700

    Add logger

--- src/main/kotlin/com/example/coursecatalogservice/controller/GreetingsController.kt
+++ src/main/kotlin/com/example/coursecatalogservice/controller/GreetingsController.kt
@@ -1,6 +1,8 @@
 package com.example.coursecatalogservice.controller
 
 import com.example.coursecatalogservice.service.GreetingsService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +14,13 @@ class GreetingsController(
     val greetingsService: GreetingsService
 ) {
 
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    }
 
     @GetMapping("/{name}")
     fun greet(@PathVariable name: String): String {
+        log.info("name = {}", name)
         return greetingsService.greet(name)
     }
 }
