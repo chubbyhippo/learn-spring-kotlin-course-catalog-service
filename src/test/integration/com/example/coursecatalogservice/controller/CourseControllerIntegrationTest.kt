0abafecfe836c82ccc4e881f2e7be58917781ea2
ch <mkmsacc@outlook.com>
2022-03-23 12:06:12 +0700

    Add CourseControllerIntegrationTest

---
+++ src/test/integration/com/example/coursecatalogservice/controller/CourseControllerIntegrationTest.kt
@@ -0,0 +1,37 @@
+package com.example.coursecatalogservice.controller
+
+import com.example.coursecatalogservice.dto.CourseDto
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebClient
+class CourseControllerIntegrationTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+        val courseDto = CourseDto(null, "Hippo study", "Hippo")
+
+        val returnResult = webTestClient.post()
+            .uri("/v1/courses")
+            .bodyValue(courseDto)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue{
+            returnResult!!.id !=null
+        }
+    }
+}
