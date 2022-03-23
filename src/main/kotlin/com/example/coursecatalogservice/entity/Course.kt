c539eaafd0846c6dbb7416ccb972026618d018a5
ch <mkmsacc@outlook.com>
2022-03-23 07:53:14 +0700

    Add Course entity

---
+++ src/main/kotlin/com/example/coursecatalogservice/entity/Course.kt
@@ -0,0 +1,13 @@
+package com.example.coursecatalogservice.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "Courses")
+data class Course(
+    @Id val id: Int?,
+    val name: String,
+    val category: String
+)
