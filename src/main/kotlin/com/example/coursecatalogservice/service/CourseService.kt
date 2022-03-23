1b99a16130574ce1c552bbeaadaffd329b88185b
ch <mkmsacc@outlook.com>
2022-03-23 08:38:13 +0700

    Acc CourseService

---
+++ src/main/kotlin/com/example/coursecatalogservice/service/CourseService.kt
@@ -0,0 +1,28 @@
+package com.example.coursecatalogservice.service
+
+import com.example.coursecatalogservice.dto.CourseDto
+import com.example.coursecatalogservice.entity.Course
+import com.example.coursecatalogservice.repository.CourseRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class CourseService(val courseRepository: CourseRepository) {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    }
+
+    fun addCourse(courseDto: CourseDto): CourseDto {
+        val courseEntity = courseDto.let {
+           Course(null, it.name, it.category)
+        }
+
+        courseRepository.save(courseEntity)
+        log.info("Saved course is : $courseEntity")
+        courseEntity.let {
+            return CourseDto(it.id, it.name, it.category)
+        }
+    }
+}
