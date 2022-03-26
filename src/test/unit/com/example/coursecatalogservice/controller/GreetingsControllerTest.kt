89856835cd7a16a1228fcd383004c871ea9206df
ch <mkmsacc@outlook.com>
2022-03-26 15:49:08 +0700

    Specify classes to test

--- src/test/unit/com/example/coursecatalogservice/controller/GreetingsControllerTest.kt
+++ src/test/unit/com/example/coursecatalogservice/controller/GreetingsControllerTest.kt
@@ -9,7 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@WebMvcTest
+@WebMvcTest(controllers = [GreetingsController::class])
 internal class GreetingsControllerTest {
 
     @Autowired
