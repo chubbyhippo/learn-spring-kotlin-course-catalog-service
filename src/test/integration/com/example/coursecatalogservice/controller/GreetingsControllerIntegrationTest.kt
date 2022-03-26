9d984f760ca80f81c469a9c4b58f09ad365a2622
ch <mkmsacc@outlook.com>
2022-03-26 15:49:05 +0700

    Specify classes to test

--- src/test/integration/com/example/coursecatalogservice/controller/GreetingsControllerIntegrationTest.kt
+++ src/test/integration/com/example/coursecatalogservice/controller/GreetingsControllerIntegrationTest.kt
@@ -1,5 +1,7 @@
 package com.example.coursecatalogservice.controller
 
+import com.example.coursecatalogservice.CourseCatalogServiceApplication
+import com.example.coursecatalogservice.service.GreetingsService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
@@ -7,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+    classes = [CourseCatalogServiceApplication::class, GreetingsController::class, GreetingsService::class],
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 @ActiveProfiles("test")
 @AutoConfigureWebClient
 class GreetingsControllerIntegrationTest {
