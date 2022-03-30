2ed4dea4c7ffa003565b9a4cb5c6fca3c8dd34c8
ch <mkmsacc@outlook.com>
2022-03-30 08:15:04 +0700

    Add retrieve all course integration test

--- src/test/integration/com/example/coursecatalogservice/controller/CourseControllerIntegrationTest.kt
+++ src/test/integration/com/example/coursecatalogservice/controller/CourseControllerIntegrationTest.kt
@@ -2,8 +2,12 @@ package com.example.coursecatalogservice.controller
 
 import com.example.coursecatalogservice.CourseCatalogServiceApplication
 import com.example.coursecatalogservice.dto.CourseDto
+import com.example.coursecatalogservice.repository.CourseRepository
 import com.example.coursecatalogservice.service.CourseService
+import com.example.coursecatalogservice.util.courseEntityList
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
@@ -21,12 +25,21 @@ class CourseControllerIntegrationTest {
 
     @Autowired
     private lateinit var webTestClient: WebTestClient
+    @Autowired
+    private lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courseEntityList = courseEntityList()
+        courseRepository.saveAll(courseEntityList)
+    }
 
     @Test
-    fun addCourse() {
+    fun shouldAddCourse() {
         val courseDto = CourseDto(null, "Hippo study", "Hippo")
 
-        val returnResult = webTestClient.post()
+        val responseBody = webTestClient.post()
             .uri("/v1/courses")
             .bodyValue(courseDto)
             .exchange()
@@ -35,8 +48,21 @@ class CourseControllerIntegrationTest {
             .returnResult()
             .responseBody
 
-        Assertions.assertTrue {
-            returnResult!!.id != null
+        assertTrue {
+            responseBody!!.id != null
         }
+    }
+
+    @Test
+    fun shouldRetrieveAllCourses() {
+        val responseBody = webTestClient.get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        assertEquals(3, responseBody?.size)
     }
 }
