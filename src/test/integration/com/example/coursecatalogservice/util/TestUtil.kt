fe855f94f7b367c740502f97374a0d19005c3ce4
ch <mkmsacc@outlook.com>
2022-03-30 08:14:26 +0700

    Add TestUtil

---
+++ src/test/integration/com/example/coursecatalogservice/util/TestUtil.kt
@@ -0,0 +1,21 @@
+package com.example.coursecatalogservice.util
+
+import com.example.coursecatalogservice.entity.Course
+
+fun courseEntityList() = listOf(
+    Course(
+        null,
+        "Hippo development",
+        "Development"
+    ),
+    Course(
+        null,
+        "Hippo love theory",
+        "Psychology"
+    ),
+    Course(
+        null,
+        "Hippo for Java developers",
+        "Development"
+    )
+)
