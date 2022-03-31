35c9712827aaa58b08b7f1d23b2ec546d6a9bc8e
ch <mkmsacc@outlook.com>
2022-03-31 07:37:32 +0700

    Add update Course service

--- src/main/kotlin/com/example/coursecatalogservice/service/CourseService.kt
+++ src/main/kotlin/com/example/coursecatalogservice/service/CourseService.kt
@@ -2,6 +2,7 @@ package com.example.coursecatalogservice.service
 
 import com.example.coursecatalogservice.dto.CourseDto
 import com.example.coursecatalogservice.entity.Course
+import com.example.coursecatalogservice.exception.CourseNotFoundException
 import com.example.coursecatalogservice.repository.CourseRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -31,5 +32,20 @@ class CourseService(val courseRepository: CourseRepository) {
             .map {
                 CourseDto(it.id, it.name, it.category)
             }
+    }
+
+    fun updateCourse(courseId: Int, courseDto: CourseDto): CourseDto {
+        val existingCourse = courseRepository.findById(courseId)
+        return if (existingCourse.isPresent) {
+            existingCourse.get()
+                .let {
+                    it.name = courseDto.name
+                    it.category = courseDto.category
+                    courseRepository.save(it)
+                    CourseDto(it.id, it.name, it.category)
+                }
+        } else {
+            throw CourseNotFoundException("No course found for the passed in Id : $courseId")
+        }
     }
 }
