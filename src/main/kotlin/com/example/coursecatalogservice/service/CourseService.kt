d7d6c8df281fb797d133174a38fc9298bded9616
ch <mkmsacc@outlook.com>
2022-04-19 11:02:33 +0700

    Add deletion service

--- src/main/kotlin/com/example/coursecatalogservice/service/CourseService.kt
+++ src/main/kotlin/com/example/coursecatalogservice/service/CourseService.kt
@@ -29,21 +29,33 @@ class CourseService(val courseRepository: CourseRepository) {
 
     fun retrieveAllCourses(): List<CourseDto> {
         return courseRepository.findAll()
-            .map {
-                CourseDto(it.id, it.name, it.category)
-            }
+                .map {
+                    CourseDto(it.id, it.name, it.category)
+                }
     }
 
     fun updateCourse(courseId: Int, courseDto: CourseDto): CourseDto {
         val existingCourse = courseRepository.findById(courseId)
         return if (existingCourse.isPresent) {
             existingCourse.get()
-                .let {
-                    it.name = courseDto.name
-                    it.category = courseDto.category
-                    courseRepository.save(it)
-                    CourseDto(it.id, it.name, it.category)
-                }
+                    .let {
+                        it.name = courseDto.name
+                        it.category = courseDto.category
+                        courseRepository.save(it)
+                        CourseDto(it.id, it.name, it.category)
+                    }
+        } else {
+            throw CourseNotFoundException("No course found for the passed in Id : $courseId")
+        }
+    }
+
+    fun deleteCourse(courseId: Int) {
+        val existingCourse = courseRepository.findById(courseId)
+        if (existingCourse.isPresent) {
+            existingCourse.get()
+                    .let {
+                        courseRepository.deleteById(courseId)
+                    }
         } else {
             throw CourseNotFoundException("No course found for the passed in Id : $courseId")
         }
