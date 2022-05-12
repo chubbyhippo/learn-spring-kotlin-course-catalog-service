d4aa35e12fc79e6a9172586a1304dd443673af6e
ch <mkmsacc@outlook.com>
2022-05-12 08:30:05 +0700

    Implement shouldAddCourse test

--- src/test/unit/com/example/coursecatalogservice/controller/CourseControllerTest.kt
+++ src/test/unit/com/example/coursecatalogservice/controller/CourseControllerTest.kt
@@ -1,7 +1,12 @@
 package com.example.coursecatalogservice.controller
 
+import com.example.coursecatalogservice.dto.CourseDto
 import com.example.coursecatalogservice.service.CourseService
+import com.example.coursecatalogservice.util.courseDto
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -17,7 +22,23 @@ internal class CourseControllerTest {
     lateinit var courseService: CourseService
 
     @Test
-    fun addCourse() {
+    fun shouldAddCourse() {
+        val courseDto = CourseDto(null, "Hippo kotlin development", "Hippo")
+        `when`(courseService.addCourse(courseDto)).thenReturn(courseDto(id = 1))
+
+        val savedCourseDto = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDto)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDto::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedCourseDto!!.id != null
+        }
     }
 
     @Test
