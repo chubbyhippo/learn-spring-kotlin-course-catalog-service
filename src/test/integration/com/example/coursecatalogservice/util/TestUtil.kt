2dfdfd8c91442df4ada956a1d5a95ebc7b33f0f1
ch <mkmsacc@outlook.com>
2022-05-12 08:29:33 +0700

    Update TestUtil.kt

--- src/test/integration/com/example/coursecatalogservice/util/TestUtil.kt
+++ src/test/integration/com/example/coursecatalogservice/util/TestUtil.kt
@@ -1,5 +1,6 @@
 package com.example.coursecatalogservice.util
 
+import com.example.coursecatalogservice.dto.CourseDto
 import com.example.coursecatalogservice.entity.Course
 
 fun courseEntityList() = listOf(
@@ -18,4 +19,16 @@ fun courseEntityList() = listOf(
         "Hippo for Java developers",
         "Development"
     )
+)
+
+fun courseDto(
+    id: Int? = null,
+    name: String = "Hippo love theory",
+    category: String = "Hippo ",
+//    instructorId: Int? = 1
+) = CourseDto(
+    id,
+    name,
+    category,
+//    instructorId
 )
