15e123ea9ae1d325e670f927986e1a27ab67fe61
ch <mkmsacc@outlook.com>
2022-05-13 07:27:00 +0700

    Add controller validations

--- src/main/kotlin/com/example/coursecatalogservice/controller/CourseController.kt
+++ src/main/kotlin/com/example/coursecatalogservice/controller/CourseController.kt
@@ -3,15 +3,18 @@ package com.example.coursecatalogservice.controller
 import com.example.coursecatalogservice.dto.CourseDto
 import com.example.coursecatalogservice.service.CourseService
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v1/courses")
+@Validated
 class CourseController(val courseService: CourseService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun addCourse(@RequestBody courseDto: CourseDto): CourseDto {
+    fun addCourse(@RequestBody @Valid courseDto: CourseDto): CourseDto {
         return courseService.addCourse(courseDto)
     }
 
