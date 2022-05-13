a40dcd4164199e442194298754570b1a7ff5b5fe
ch <mkmsacc@outlook.com>
2022-05-13 07:26:51 +0700

    Add validations

--- src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
+++ src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
@@ -1,7 +1,11 @@
 package com.example.coursecatalogservice.dto
 
+import javax.validation.constraints.NotBlank
+
 data class CourseDto(
     val id: Int?,
+    @get:NotBlank(message = "courseDto.name must not be blank")
     val name: String,
+    @get:NotBlank(message = "courseDto.category must not be blank")
     val category: String
 )
