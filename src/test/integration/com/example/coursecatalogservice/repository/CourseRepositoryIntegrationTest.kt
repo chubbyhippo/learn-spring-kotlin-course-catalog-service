99c0c0cb494c8c7e2e76fe378e0f8d971fc99a7b
ch <mkmsacc@outlook.com>
2022-05-16 14:34:05 +0700

    Add CourseRepositoryIntegrationTest.kt

---
+++ src/test/integration/com/example/coursecatalogservice/repository/CourseRepositoryIntegrationTest.kt
@@ -0,0 +1,28 @@
+package com.example.coursecatalogservice.repository
+
+import com.example.coursecatalogservice.util.courseEntityList
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+class CourseRepositoryIntegrationTest {
+
+    @Autowired
+    private lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courseEntityList = courseEntityList()
+        courseRepository.saveAll(courseEntityList)
+    }
+
+    @Test
+    fun shouldFindByNameContaining() {
+        val courses = courseRepository.findByNameContaining("Hippo")
+        Assertions.assertEquals(3, courses.size)
+    }
+}
