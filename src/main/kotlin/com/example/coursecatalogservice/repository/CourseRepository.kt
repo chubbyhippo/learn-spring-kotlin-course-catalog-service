1ec193410fc1578d9e634df4584725fef4c5712d
ch <mkmsacc@outlook.com>
2022-05-16 14:41:47 +0700

    Add shouldFindByName method

--- src/main/kotlin/com/example/coursecatalogservice/repository/CourseRepository.kt
+++ src/main/kotlin/com/example/coursecatalogservice/repository/CourseRepository.kt
@@ -2,7 +2,12 @@ package com.example.coursecatalogservice.repository
 
 import com.example.coursecatalogservice.entity.Course
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface CourseRepository : JpaRepository<Course, Int> {
     fun findByNameContaining(courseName: String): List<Course>
+
+    @Query(value = "select * from courses where name like ?1", nativeQuery = true)
+    fun findCourseByName(courseName: String): List<Course>
+
 }
