ae32970a811d8202204500c0c3eeaa731d329efe
ch <mkmsacc@outlook.com>
2022-05-16 18:48:36 +0700

    Add shouldAddCourse unit test

---
+++ src/test/unit/com/example/coursecatalogservice/service/CourseServiceTest.kt
@@ -0,0 +1,38 @@
+package com.example.coursecatalogservice.service
+
+import com.example.coursecatalogservice.dto.CourseDto
+import com.example.coursecatalogservice.entity.Course
+import com.example.coursecatalogservice.repository.CourseRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+internal class CourseServiceTest {
+    @Mock
+    lateinit var courseRepository: CourseRepository
+
+    @InjectMocks
+    lateinit var courseService: CourseService
+
+    @Test
+    fun shouldAddCourse() {
+        val courseDto = CourseDto(null, "Hippo kotlin development", "Hippo")
+        val courseEntity = courseDto.let {
+            Course(null, it.name, it.category)
+        }
+        Mockito.`when`(courseRepository.save(courseEntity))
+            .thenReturn(Course(1, courseEntity.name, courseEntity.category))
+
+        val addedCourse = courseService.addCourse(courseDto)
+        println(addedCourse)
+
+        Assertions.assertNotNull(addedCourse.id)
+
+    }
+
+}
