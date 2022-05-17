ca8f153e21d2317e029b1e4c53d3a936c6a31f61
ch <mkmsacc@outlook.com>
2022-05-17 08:29:54 +0700

    Add shouldUpdateCourse unit test

--- src/test/unit/com/example/coursecatalogservice/service/CourseServiceTest.kt
+++ src/test/unit/com/example/coursecatalogservice/service/CourseServiceTest.kt
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.*
 import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
 
 @ExtendWith(MockitoExtension::class)
 internal class CourseServiceTest {
@@ -48,28 +48,30 @@ internal class CourseServiceTest {
         Assertions.assertEquals(3, courseDtos.size)
     }
 
-//    @Test
-//    fun shouldUpdateCourse() {
-//
-//        Mockito.`when`(courseRepository.findById())
-//        val updatedCourseDto = CourseDto(
-//            null,
-//            "Hello Hippo",
-//            "General"
-//        )
-//
-//        val updateCourseDto = webTestClient.put()
-//            .uri("/v1/courses/{courseId}", course.id)
-//            .bodyValue(updatedCourseDto)
-//            .exchange()
-//            .expectStatus().isOk
-//            .expectBody(CourseDto::class.java)
-//            .returnResult()
-//            .responseBody
-//
-//        Assertions.assertEquals("Hello Hippo", updateCourseDto!!.name)
-//        Assertions.assertEquals("General", updateCourseDto.category)
-//
-//    }
+    @Test
+    fun shouldUpdateCourse() {
+
+        val courseDto = CourseDto(
+            null,
+            "Hello Hippo",
+            "General"
+        )
+        `when`(courseRepository.findById(anyInt())).thenReturn(
+            Optional.of(
+                Course(
+                    1,
+                    courseDto.name,
+                    courseDto.category
+                )
+            )
+        )
+
+        val (id, name, category) = courseService.updateCourse(1, courseDto)
+
+        Assertions.assertEquals(1, id)
+        Assertions.assertEquals("Hello Hippo", name)
+        Assertions.assertEquals("General", category)
+
+    }
 
 }
