acc04484a6f6d6ecb0b4fee66db35350b2362118
ch <mkmsacc@outlook.com>
2022-05-18 08:02:02 +0700

    Add InstructorDto

---
+++ src/main/kotlin/com/example/coursecatalogservice/dto/InstructorDto.kt
@@ -0,0 +1,10 @@
+package com.example.coursecatalogservice.dto
+
+import javax.validation.constraints.NotBlank
+
+data class InstructorDto(
+    val id: Int?,
+    @get:NotBlank(message = "instructorDto.name must not be blank")
+    var name: String
+)
+
