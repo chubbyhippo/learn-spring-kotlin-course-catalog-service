effff67039bd04d894ed8d5c46e2b8ac7b6a69bb
ch <mkmsacc@outlook.com>
2022-05-23 07:58:27 +0700

    Add InstructorService

---
+++ src/main/kotlin/com/example/coursecatalogservice/service/InstructorService.kt
@@ -0,0 +1,20 @@
+package com.example.coursecatalogservice.service
+
+import com.example.coursecatalogservice.dto.InstructorDto
+import com.example.coursecatalogservice.entity.Instructor
+import com.example.coursecatalogservice.repository.InstructorRepository
+import org.springframework.stereotype.Service
+
+@Service
+class InstructorService(val instructorRepository: InstructorRepository) {
+    fun createInstructor(instructorDto: InstructorDto): InstructorDto {
+        val instructor = instructorDto.let {
+            Instructor(it.id, it.name)
+        }
+        val save = instructorRepository.save(instructor)
+        return save.let {
+            InstructorDto(it.id, it.name)
+        }
+    }
+
+}
