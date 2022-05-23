a694dd740e93a5cb79b80f598db28cb2c16fda11
ch <mkmsacc@outlook.com>
2022-05-23 07:57:45 +0700

    Add InstructorController

---
+++ src/main/kotlin/com/example/coursecatalogservice/controller/InstructorController.kt
@@ -0,0 +1,23 @@
+package com.example.coursecatalogservice.controller
+
+import com.example.coursecatalogservice.dto.InstructorDto
+import com.example.coursecatalogservice.service.InstructorService
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/instructors")
+@Validated
+class InstructorController(val instructorService: InstructorService) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createInstructor(@RequestBody instructorDto: InstructorDto) {
+        return instructorService.createInstructor(instructorDto)
+    }
+}
