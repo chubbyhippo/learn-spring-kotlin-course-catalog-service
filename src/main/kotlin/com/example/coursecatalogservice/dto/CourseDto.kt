d8fe6e22ce42897941e1c0881fec06554a5204cd
ch <mkmsacc@outlook.com>
2022-05-24 07:42:49 +0700

    Update CourseDto

--- src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
+++ src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
@@ -7,5 +7,7 @@ data class CourseDto(
     @get:NotBlank(message = "courseDto.name must not be blank")
     val name: String,
     @get:NotBlank(message = "courseDto.category must not be blank")
-    val category: String
+    val category: String,
+    @get:NotBlank(message = "courseDto.instructorId must not be blank")
+    val instructorId: Int? = null
 )
