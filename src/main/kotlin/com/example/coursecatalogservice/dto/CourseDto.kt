f15863401a828d42fe569e11abf91f39ebcefa7f
ch <mkmsacc@outlook.com>
2022-05-24 08:11:54 +0700

    Change not blank to not null

--- src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
+++ src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
@@ -1,6 +1,7 @@
 package com.example.coursecatalogservice.dto
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class CourseDto(
     val id: Int?,
@@ -8,6 +9,6 @@ data class CourseDto(
     val name: String,
     @get:NotBlank(message = "courseDto.category must not be blank")
     val category: String,
-    @get:NotBlank(message = "courseDto.instructorId must not be blank")
+    @get:NotNull(message = "courseDto.instructorId must not be null")
     val instructorId: Int? = null
 )
