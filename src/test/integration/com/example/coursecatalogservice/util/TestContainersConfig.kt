0db72067404e3244f18510c0f03554d0b8f1eb5b
ch <mkmsacc@outlook.com>
2022-05-26 08:25:08 +0700

    Add TestContainersConfig

---
+++ src/test/integration/com/example/coursecatalogservice/util/TestContainersConfig.kt
@@ -0,0 +1,29 @@
+package com.example.coursecatalogservice.util
+
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+@Testcontainers
+open class TestContainersConfig {
+    companion object {
+
+        @Container
+        val postgresDB = PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:14")).apply {
+            withDatabaseName("testdb")
+            withUsername("postgres")
+            withPassword("secret")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresDB::getJdbcUrl)
+            registry.add("spring.datasource.username", postgresDB::getUsername)
+            registry.add("spring.datasource.password", postgresDB::getPassword)
+        }
+    }
+}
