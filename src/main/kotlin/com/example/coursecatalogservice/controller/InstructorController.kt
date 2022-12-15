fe16ed5a277216bf8931392a43d1499e161e84f8
ch <mkmsacc@outlook.com>
2022-12-15 11:57:15 +0700

    Fix imports intructor controller

--- src/main/kotlin/com/example/coursecatalogservice/controller/InstructorController.kt
+++ src/main/kotlin/com/example/coursecatalogservice/controller/InstructorController.kt
@@ -2,6 +2,7 @@ package com.example.coursecatalogservice.controller
 
 import com.example.coursecatalogservice.dto.InstructorDto
 import com.example.coursecatalogservice.service.InstructorService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v1/instructors")
