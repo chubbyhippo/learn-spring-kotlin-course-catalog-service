3101d335cce2c90aaa1cdbbbc8cde23a3401398d
ch <mkmsacc@outlook.com>
2022-12-15 11:56:28 +0700

    Fix imports Course Dto

--- src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
+++ src/main/kotlin/com/example/coursecatalogservice/dto/CourseDto.kt
@@ -1,7 +1,8 @@
 package com.example.coursecatalogservice.dto
 
-import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
 
 data class CourseDto(
     val id: Int?,
